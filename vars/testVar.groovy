2bdf8e130cc28c6717d4376860e3e07851821714
baharclerode <baharclerode@users.noreply.github.com>
2017-07-10 15:25:50 -0500

    Update testVar.groovy

--- vars/testVar.groovy
+++ vars/testVar.groovy
@@ -3,10 +3,18 @@ def call(Closure body) {
     echo this.class.name
     
     def config = [:]
+    
+    config.preBuild = { config.preBuildClosure = it }
+    
     body.delegate = config
     body.resolveStrategy = Closure.DELEGATE_FIRST
     
     body()
+    
+    echo config.someValue
+    
+    def preBuildClosure = config.preBuildClosure
+    preBuildClosure()
 
 
 }
