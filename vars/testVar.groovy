646fca6101caf0a9221ce418a3cfe69322c6055b
baharclerode <baharclerode@users.noreply.github.com>
2017-07-10 15:51:29 -0500

    Update testVar.groovy

--- vars/testVar.groovy
+++ vars/testVar.groovy
@@ -1,20 +1,21 @@
+class testVar implements Serializable {
+    def call(Closure body) {
+        echo this.class.name
 
-def call(Closure body) {
-    echo this.class.name
-    
-    def config = [:]
-    
-    config.preBuild = { config.preBuildClosure = it }
-    
-    body.delegate = config
-    body.resolveStrategy = Closure.DELEGATE_FIRST
-    
-    body()
-    
-    echo config.someValue
-    
-    def preBuildClosure = config.preBuildClosure
-    preBuildClosure()
+        def config = [:]
+
+        config.preBuild = { config.preBuildClosure = it }
+
+        body.delegate = config
+        body.resolveStrategy = Closure.DELEGATE_FIRST
+
+        body()
+
+        echo config.someValue
+
+        def preBuildClosure = config.preBuildClosure
+        preBuildClosure()
 
 
+    }
 }
