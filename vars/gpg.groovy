07355d6ad627fb0bbb910e6a28a4ad3741e8ae0c
Bryan Harclerode <baharclerode@devdragon.net>
2018-02-19 00:25:02 -0600

    Update gpg.groovy

--- vars/gpg.groovy
+++ vars/gpg.groovy
@@ -5,7 +5,11 @@ def call(String keyfileId, Closure callback) {
     withEnv(["GNUPGHOME=/tmp/gnupg"]) {
         sh "mkdir -p $GNUPGHOME && chmod 700 $GNUPGHOME"
         withCredentials([file(credentialsId: keyfileId, variable: 'GPG_SIGNING_KEY_FILE')]) {
-            def keyIdLine = sh(returnStdOut: true, script: 'gpg --allow-secret-key-import --import $GPG_SIGNING_KEY_FILE | grep "secret key imported"')
+            def keyIdShort = sh(returnStdOut: true, script: 'gpg --allow-secret-key-import --import $GPG_SIGNING_KEY_FILE 2>&1 | grep \'secret key imported\' | cut -d \' \' -f 3 | cut -d \':\' -f 1')
+            def keyIdFull = sh(returnStdOut: true, script: "gpg --list-keys | grep '${keyIdShort}'")
+            callback.delegate = this
+            callback.resolveStrategy = Closure.DELEGATE_FIRST
+            callback(keyIdFull)
         }    
     }
 }
