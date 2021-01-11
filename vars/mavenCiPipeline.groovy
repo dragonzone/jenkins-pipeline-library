1e5044c04e19e73a55de33a09018fd12dd00435f
Bryan Harclerode <baharclerode@devdragon.net>
2021-01-10 23:52:17 -0600

    Fix version regex

--- vars/mavenCiPipeline.groovy
+++ vars/mavenCiPipeline.groovy
@@ -61,7 +61,7 @@ def call(Closure closure) {
                     echo "Artifact ID matches"
                     def lastVersion = lastTag.substring(artifactId.length()+1)
                     echo "Last Version: ${lastVersion}"
-                    def versionPattern = Pattern.compile(Pattern.quote(versionTemplate).replaceAll(Pattern.quote('\\$\\{revision\\}'), '(?<revision>\\d+)').replaceAll(Pattern.quote('\\$\\{sha1\\}'), '[0-9a-fA-F]+'))
+                    def versionPattern = Pattern.compile(Pattern.quote(versionTemplate).replaceAll('\\Q${revision}\\E', '\\\\E(?<revision>\\d+)\\\\Q').replaceAll('\\Q${sha1}\\E', '\\\\E[0-9a-fA-F]+\\\\Q'))
                     echo "Version Pattern: ${versionPattern}"
                     def matcher = lastTag =~ versionPattern
                     if (matcher.matches()) {
